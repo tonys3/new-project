eed5376e5425e98dab154f44ebd8120d0663f3c9
user <user@hb6002>
2017-05-25 13:58:52 +0900

    am end

--- day001/src/com/hb/model/SimpleDaoTest.java
+++ day001/src/com/hb/model/SimpleDaoTest.java
@@ -16,13 +16,22 @@ import org.junit.Test;
 
 public class SimpleDaoTest {
 	static Logger log=Logger.getLogger(SimpleDaoTest.class);
-	@BeforeClass public static void setUpBeforeClass() throws Exception {}
+	static SimpleDao dao;
+	@BeforeClass public static void setUpBeforeClass() throws Exception {
+//		dao = new SimpleDao(false);
+	}
 
-	@AfterClass public static void tearDownAfterClass() throws Exception {}
+	@AfterClass public static void tearDownAfterClass() throws Exception {
+//		dao.back();
+	}
 
-	@Before public void setUp() throws Exception {}
+	@Before public void setUp() throws Exception {
+		dao = new SimpleDao(false);
+	}
 
-	@After public void tearDown() throws Exception {}
+	@After public void tearDown() throws Exception {
+		dao.back();
+	}
 
 	@Test public void testSimpleDao() throws ClassNotFoundException, SQLException {
 		Assert.assertNotNull(new SimpleDao());
@@ -33,10 +42,30 @@ public class SimpleDaoTest {
 		List<Map<String,Object>> list=dao.selectAll();
 		Assert.assertNotNull(list);
 		Assert.assertTrue(list.size()>0);
-		Assert.assertSame(3, list.size());
+		Assert.assertSame(5, list.size());
 		for (Map<String, Object> map : list) {
-			log.debug(map.get("sabun"));
+			log.debug(map.get("sabun"));//log4j.properties
 		}
 	}
-
+	
+	@Test
+	public void testSelectOne() throws ClassNotFoundException, SQLException{
+		int sabun=1000;
+		SimpleDao dao = new SimpleDao();
+		Map<String,Object> map=dao.selectOne(sabun);
+		Assert.assertNotNull(map);
+		Assert.assertEquals(sabun, map.get("sabun"));
+		Assert.assertEquals("aaaa", map.get("name"));
+		Assert.assertEquals(1111, map.get("pay"));
+	}
+	
+	@Test
+	public void testinsertOne() throws SQLException{
+		int sabun=4000;
+		int pay=4444;
+		String name="test";
+		int result=dao.insertOne(sabun, name, pay);
+		Assert.assertSame(1, result);
+	}
+	
 }
